13d49bb51bd93d05f5b45789a42729a2d211e4d8
Juno Raihala <jcraihala@gmail.com>
2019-12-09 13:35:01 -0600

    Code for 2019 9.2. All caught up!

--- 2019/day9.py
+++ 2019/day9.py
@@ -4,6 +4,11 @@ with open('day-9-input.txt') as f:
     program = [int(x) for x in f.readline().strip().split(',')]
 
 computer = IntcodeComputer()
-outputs = computer.run(program, [1])
 
+# Part 1
+outputs = computer.run(program, [1])
+print(outputs)
+
+# Part 2
+outputs = computer.run(program, [2])
 print(outputs)
