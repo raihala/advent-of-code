c52e59ce9b73a9857c02f53c2e694e648d9d47b0
Juno Raihala <jcraihala@gmail.com>
2021-12-06 10:41:33 -0600

    Day 6

---
+++ 2021/6/main.py
@@ -0,0 +1,34 @@
+from collections import Counter
+
+DESCENDENTS_AFTER_N_DAYS = {}
+
+
+def descendents_after_n_days(days_remaining):
+    if days_remaining in DESCENDENTS_AFTER_N_DAYS:
+        return DESCENDENTS_AFTER_N_DAYS[days_remaining]
+
+    children = range(7, days_remaining, 7)
+    if len(children) == 0:
+        DESCENDENTS_AFTER_N_DAYS[days_remaining] = 0
+        return 0
+    children_days_remaining = [(days_remaining - x - 2) for x in children]
+
+    descendents = len(children) + sum([descendents_after_n_days(c) for c in children_days_remaining])
+    DESCENDENTS_AFTER_N_DAYS[days_remaining] = descendents
+    return descendents
+
+
+def calculate(initial_state, total_days):
+    effective_days = Counter([total_days + (7 - x) for x in initial_state])
+    num_fish = len(initial_state)
+    for days, multiplier in effective_days.items():
+        num_fish += multiplier * descendents_after_n_days(days)
+    return num_fish
+
+
+with open('input') as f:
+    initial_state = [int(x) for x in next(f).strip().split(',')]
+
+
+print(calculate(initial_state, 80))
+print(calculate(initial_state, 256))
