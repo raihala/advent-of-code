fd81a1dcadf7bd4173e6ad9fbaec0e9b7c552401
Juno Raihala <jcraihala@gmail.com>
2021-12-06 10:56:19 -0600

    Update day 6

--- 2021/6/main.py
+++ 2021/6/main.py
@@ -2,15 +2,13 @@ DESCENDENTS_AFTER_N_DAYS = {}
 
 
 def descendents_after_n_days(days_remaining):
+    if days_remaining < 7:
+        return 0
     if days_remaining in DESCENDENTS_AFTER_N_DAYS:
         return DESCENDENTS_AFTER_N_DAYS[days_remaining]
 
     children = range(7, days_remaining, 7)
-    if len(children) == 0:
-        DESCENDENTS_AFTER_N_DAYS[days_remaining] = 0
-        return 0
     children_days_remaining = [(days_remaining - x - 2) for x in children]
-
     descendents = len(children) + sum([descendents_after_n_days(c) for c in children_days_remaining])
     DESCENDENTS_AFTER_N_DAYS[days_remaining] = descendents
     return descendents
